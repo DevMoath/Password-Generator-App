5bc1c5c081998497d010cf674f7f769192a17f37
DevMoath <moath.alhajrii@gmail.com>
2020-03-15 00:02:43 +0300

    update meta data, use npm and webpack for dependencies

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Page from "./Page";
+import PasswordGenerator from "./PasswordGenerator";
+
+window.$ = require("jquery");
+require("bootstrap/dist/css/bootstrap.min.css");
+require("@fortawesome/fontawesome-free/js/fontawesome.min");
+require("@fortawesome/fontawesome-free/js/brands.min");
+require("@fortawesome/fontawesome-free/js/solid.min");
+require("./index.css");
+window.swal = require("sweetalert2");
+
+window.onload = () => {
+    const page = new Page();
+    const app = new PasswordGenerator(page);
+    page.init(app);
+    page.generate.click();
+};
